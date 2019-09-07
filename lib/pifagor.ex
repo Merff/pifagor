520a9f26a24d1155e0634ca77e0fd728295332f1
Alex Mesh <merff.555@gmail.com>
2019-09-07 18:42:03 +0700

    initial commit

---
+++ lib/pifagor.ex
@@ -0,0 +1,18 @@
+defmodule Pifagor do
+  @moduledoc """
+  Documentation for Pifagor.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Pifagor.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
